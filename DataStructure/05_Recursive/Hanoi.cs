5afda39cf0030d7be9e3db7edcccbdf839ddf2a2
JeongGil Lee <36697521+JeongGil@users.noreply.github.com>
2020-11-25 23:55:21 +0900

    Recursive

---
+++ DataStructure/05_Recursive/Hanoi.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05_Recursive
+{
+    class Hanoi
+    {
+        private List<int> rodA = new List<int>();
+        private List<int> rodB = new List<int>();
+        private List<int> rodC = new List<int>();
+        private int size;
+
+        public void PrintTowers()
+        {
+            Console.Clear();
+
+            Console.Write("A: ");
+            foreach(var disk in rodA)
+            {
+                Console.Write("{0} ", disk);
+            }
+            Console.Write("\n");
+
+            Console.Write("B: ");
+            foreach (var disk in rodB)
+            {
+                Console.Write("{0} ", disk);
+            }
+            Console.Write("\n");
+
+            Console.Write("C: ");
+            foreach (var disk in rodC)
+            {
+                Console.Write("{0} ", disk);
+            }
+            Console.Write("\n");
+        }
+
+        public void Run()
+        {
+            Solve(rodA, rodC, rodB, size);
+        }
+
+        private void Move(List<int> src, List<int> dest)
+        {
+            string temp=Console.ReadLine();
+            if (temp.Equals("x"))
+            {
+                Environment.Exit(0);
+            }
+
+            dest.Add(src[src.Count - 1]);
+            src.RemoveAt(src.Count - 1);
+            PrintTowers();
+        }
+
+        private void Solve(List<int> src, List<int> dest, List<int> temp, int n)
+        {
+            if (n.Equals(1))
+            {
+                Move(src, dest);
+            }
+            else
+            {
+                Solve(src, temp, dest, n - 1);
+                Move(src, dest);
+                Solve(temp, dest, src, n - 1);
+            }
+        }
+
+        public Hanoi(int size)
+        {
+            if (size < 1)
+            {
+                size = 1;
+            }
+            this.size = size;
+
+            for (; !size.Equals(0); size--)
+            {
+                rodA.Add(size);
+            }
+        }
+    }
+}
---
+++ DataStructure/05_Recursive/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _05_Recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Fibonacci
+            for (uint i = 1; i < 10; i++)
+            {
+                Console.Write("{0}, ", Fibonacci_Recursive(i));
+            }
+            Console.Write("\n");
+
+            for (uint i = 1; i < 10; i++)
+            {
+                Console.Write("{0}, ", Fibonacci_NonRecursive(i));
+            }
+            Console.WriteLine("\n");
+            #endregion
+
+            #region Hanoi
+            Console.Write("Input the number of disks: ");
+            int diskCount = Convert.ToInt32(Console.ReadLine());
+            Hanoi hanoi = new Hanoi(diskCount);
+
+            hanoi.PrintTowers();
+
+            hanoi.Run();
+            #endregion
+        }
+
+        static uint Fibonacci_Recursive(uint index)
+        {
+            if (index.Equals(1) || index.Equals(2))
+            {
+                return 1;
+            }
+
+            return Fibonacci_Recursive(index - 2) + Fibonacci_Recursive(index - 1);
+        }
+
+        static uint Fibonacci_NonRecursive(uint index)
+        {
+            if (index.Equals(1) || index.Equals(2))
+            {
+                return 1;
+            }
+
+            uint a2 = 1;
+            uint a1 = 1;
+            uint a0;
+            uint counter = 3;
+            while (true)
+            {
+                a0 = a2 + a1;
+                if (counter.Equals(index))
+                {
+                    return a0;
+                }
+                else
+                {
+                    a2 = a1;
+                    a1 = a0;
+
+                    counter++;
+                }
+            }
+        }
+    }
+}
